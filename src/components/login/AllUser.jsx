3d299df2f1b241958e9a0a1c80f643744a5564cc
LINO ADRIÁN PÁEZ REYES <54821048+Adriianpr919@users.noreply.github.com>
2023-02-23 17:15:10 -0500

    varios cambios.

--- src/components/login/AllUser.jsx
+++ src/components/login/AllUser.jsx
@@ -32,50 +32,57 @@ const AllUser = ({ user }) => {
 
   return (
     <>
-      <table className="table table-sm mb-0 overflow-hidden data-table fs--1" data-options="{&quot;responsive&quot;:false,&quot;pagingType&quot;:&quot;simple&quot;,&quot;lengthChange&quot;:false,&quot;searching&quot;:false,&quot;pageLength&quot;:8,&quot;language&quot;:{&quot;info&quot;:&quot;_START_ to _END_ Items of _TOTAL_&quot;}}" key={user._id}>
-        <thead className="bg-200 text-900">
-          <tr>
-            <th className="sort pe-1 align-middle white-space-nowrap">
-              <span className="badge rounded-pill badge-soft-primary" style={{ fontSize: "15px" }}>USUARIO.</span>
-            </th>
-            <th className="no-sort pe-1 align-middle data-table-row-action" />
-          </tr>
-        </thead>
-        <tbody className="list" id="table-simple-pagination-body">
-          <tr className="btn-reveal-trigger">
-            <th className="align-middle white-space-nowrap">
-              <span className="badge rounded-pill badge-soft-secondary" style={{ fontSize: "15px" }}>{user.username}</span>
-            </th>
-            <td className="align-middle white-space-nowrap text-end">
-              <div className="dropstart font-sans-serif position-static d-inline-block">
-                <button className="btn btn-link text-600 btn-sm dropdown-toggle btn-reveal float-end badge rounded-pill badge-soft-dark" type="button"
-                  id="dropdown-simple-pagination-table-item-0" data-bs-toggle="dropdown" data-boundary="window"
-                  aria-haspopup="true" aria-expanded="false" data-bs-reference="parent">
-                  <span className="fas fa-ellipsis-h fs--1" />
-                </button>
-                <div className="dropdown-menu dropdown-menu-end border py-2"
-                  aria-labelledby="dropdown-simple-pagination-table-item-0">
-                  <span className="badge badge rounded-pill badge-soft-secondary text-center" style={{ fontSize: "15px", textAlign: "justify" }}>
-                    <FontAwesomeIcon icon={faEye} onClick={() => setOpenViewUser(true)} /> <FontAwesomeIcon icon="fa-solid fa-hand-point-left" /> PRESIONA EL ICONO.
-                  </span>
-                  <div className="dropdown-divider" />
-                  <span className="badge badge rounded-pill badge-soft-success text-center" style={{ fontSize: "15px", textAlign: "justify" }}>
-                    <FontAwesomeIcon icon={faEdit} onClick={() => setOpenEditUser(true)} /> <FontAwesomeIcon icon="fa-solid fa-hand-point-left" /> PRESIONA EL ICONO.
-                  </span>
-                  <div className="dropdown-divider" />
-                  <span className="badge badge rounded-pill badge-soft-warning text-center" style={{ fontSize: "15px", textAlign: "justify" }}>
-                    <FontAwesomeIcon icon={faKey} onClick={() => setOpenChangePassUser(true)} /> <FontAwesomeIcon icon="fa-solid fa-hand-point-left" /> PRESIONA EL ICONO.
-                  </span>
-                  <div className="dropdown-divider" />
-                  <span className="badge badge rounded-pill badge-soft-danger text-center" style={{ fontSize: "15px", textAlign: "justify" }}>
-                    <FontAwesomeIcon icon={faTrash} onClick={handlerDeleteUser} /> <FontAwesomeIcon icon="fa-solid fa-hand-point-left" /> PRESIONA EL ICONO.
-                  </span>
+      <>
+        <div className="card mb-4" key={user._id}>
+          <div className="card-body">
+            <div className="row">
+              <div className="mb-4 col-md-6 col-lg-4">
+                <div className="border rounded-1 h-100 d-flex flex-column justify-content-between pb-3">
+                  <div className="overflow-hidden">
+                    <div className="p-3">
+                      <h5 className="fs-0">
+                        <p className="text-dark">
+                          Usuario. :* ({user.username})
+                        </p>
+                      </h5>
+                      <hr className="my-4" />
+                      <h5 className="fs-0">
+                        <p className="text-dark">
+                          Correo. :* {user.email}
+                        </p>
+                      </h5>
+                    </div>
+                  </div>
+                  <div className="d-flex flex-between-center px-3">
+                    <div>
+                      <>
+                        <span className="badge badge badge-soft-secondary text-center btn btn-sm btn-falcon-default me-2" style={{ fontSize: "15px", textAlign: "justify" }} title="Ver Usuarios.">
+                          <FontAwesomeIcon icon={faEye} onClick={() => setOpenViewUser(true)} />
+                        </span>
+                      </>
+                      <>
+                        <span className="badge badge badge-soft-success text-center btn btn-sm btn-falcon-default me-2" style={{ fontSize: "15px", textAlign: "justify" }} title="Editar Usuarios.">
+                          <FontAwesomeIcon icon={faEdit} onClick={() => setOpenEditUser(true)} />
+                        </span>
+                      </>
+                      <>
+                        <span className="badge badge badge-soft-warning text-center btn btn-sm btn-falcon-default me-2" style={{ fontSize: "15px", textAlign: "justify" }} title="Cambiar Claves.">
+                          <FontAwesomeIcon icon={faKey} onClick={() => setOpenChangePassUser(true)} />
+                        </span>
+                      </>
+                      <>
+                        <span className="badge badge badge-soft-danger text-center btn btn-sm btn-falcon-default me-2" style={{ fontSize: "15px", textAlign: "justify" }} title="Borrar Usuarios.">
+                          <FontAwesomeIcon icon={faTrash} onClick={handlerDeleteUser} />
+                        </span>
+                      </>
+                    </div>
+                  </div>
                 </div>
               </div>
-            </td>
-          </tr>
-        </tbody>
-      </table>
+            </div>
+          </div>
+        </div>
+      </>
       {openViewUser && <ViewUser user={user} setOpenViewUser={setOpenViewUser} />}
       {openEditUser && <Edituser user={user} setOpenEditUser={setOpenEditUser} />}
       {openChangePassUser && <ChangePassUser user={user} setOpenChangePassUser={setOpenChangePassUser} />}
